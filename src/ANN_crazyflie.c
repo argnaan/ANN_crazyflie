27bc213a7921333ff1a7541e026b664cce0bdc23
argnaan <andrea.argnani2002@gmail.com>
2025-04-01 14:00:02 +0200

    aggiornamento martedì 1_04_25

--- src/ANN_crazyflie.c
+++ src/ANN_crazyflie.c
@@ -28,13 +28,14 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include "ANN_crazyflie.h"
 
 #include "app.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define DEBUG_MODULE "ANN_crazyflie"
+// #define DEBUG_MODULE "ANN_crazyflie"
 #include "debug.h"
 
 #include "log.h"
@@ -44,7 +45,15 @@
 #include "pmw3901.h" // Optical flow deck driver
 #include "deck.h"    // For deck GPIO pin definitions
 
+#include "sensors.h"
+// #include "state_estimator.h"
+#include "stabilizer.h"
+
+
+
 #define NCS_PIN DECK_GPIO_IO3
+#define INPUT_SIZE 17         // dimensione del buffer di input
+
 
 
 void appMain() {
@@ -60,7 +69,12 @@ void appMain() {
 
     float inputData[ INPUT_SIZE ];
     
-    readSensors(*inputData);
+    readSensors(inputData);
+
+    for(int i=0; i<6; i++){
+      DEBUG_PRINT("%f\t", (double) inputData[i]);
+    }
+    DEBUG_PRINT("\n");
 
     // preProcessing(...); ?
 
@@ -68,25 +82,29 @@ void appMain() {
 
 
 
-    vTaskDelay(M2T(2000));
-    DEBUG_PRINT("Hello World!\n");
+    vTaskDelay(M2T(100));
   }
 }
 
 void readSensors(float *data){
+  
   // flow deck
   motionBurst_t currentMotion;
   pmw3901ReadMotion(NCS_PIN, &currentMotion);
   data[0] = -currentMotion.deltaX;
   data[1] = -currentMotion.deltaY;
-  data[2] = currentMotion.squal; // Surface quality
+  data[2] = currentMotion.squal;                // Surface quality
   
   // accelerazione
+  Axis3f acc;
+  sensorsReadAcc(&acc);
+  data[3] = acc.x;
+  data[4] = acc.y;
+  data[5] = acc.z;
 
   // altitudine
-
+  
   // pitch roll yaw
 
-  // telecamera??
 }
 
---
+++ src/ANN_crazyflie.h
@@ -0,0 +1,1 @@
+void readSensors(float *data);
